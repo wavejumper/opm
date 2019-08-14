75ae2c1ed3733d74fa24bca855df18c5248b91d1
Thomas Crowley <crowley@kibu.com.au>
2019-08-15 00:16:16 +1000

    Do not need CORS

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,6 @@ mod logging;
 
 use std::thread;
 use actix_web::{http::header, middleware, App, HttpServer};
-use actix_cors::Cors;
 use types::{Context};
 use logging::{PdLogger};
 use log::{info, LevelFilter};
@@ -21,14 +20,6 @@ pub unsafe extern "C" fn hello_rust() {
         HttpServer::new(move || {
             let state = Context::new("/home/tscrowley/samples");
             App::new()
-                .wrap(
-                    Cors::new()
-                        .allowed_origin("http://0.0.0.0:9000")
-                        .allowed_methods(vec!["GET", "POST", "OPTIONS"])
-                        .allowed_headers(vec![header::AUTHORIZATION, header::ACCEPT])
-                        .allowed_header(header::CONTENT_TYPE)
-                        .max_age(3600)
-                )
                 .data(state)
                 .wrap(middleware::Logger::default())
         })
